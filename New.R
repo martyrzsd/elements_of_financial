e7c496e191f052cfc903cf411e0cad027b7a4771
Addasecond86 <61357287+Addasecond86@users.noreply.github.com>
2020-03-27 00:26:56 +0800

    先这样吧

---
+++ New.R
@@ -0,0 +1,54 @@
+library(fGarch)
+data <- read.csv("08年每日.csv")
+SPY <- ts(data$收盘, frequency = 251, start = c(2008, 1))
+dp <- ts(diff(SPY), frequency = 251, start = c(2008, 1))
+plot(SPY)
+plot(dp)
+cprice <- diff(data$收盘)
+
+
+m2 <- arima(cprice, order = c(3, 0, 0), seasonal = list(order = c(2, 0, 0), period = 5), include.mean = F)
+m2
+length(cprice)
+acf(cprice)
+pacf(cprice)
+
+
+m3 <- arima(cprice, seasonal = list(order = c(2, 0, 0), period = 5), include.mean = F)
+summary(m3)
+#减去季节性干扰因素
+adjcp <- cprice[11:3081] + 0.0817 * cprice[6:3076] - 0.0667 * cprice[1:3071]
+acf(adjcp)
+pacf(adjcp)
+
+
+m4 <- garchFit(~arma(3, 0) + garch(1, 1), data = adjcp, trace = F, include.mean = F)
+summary(m4)
+plot(m4)
+
+
+m5 <- garchFit(~arma(3, 0) + garch(1, 1), data = adjcp, trace = F, include.mean = F, cond.dist = "std")
+summary(m5)
+plot(m5)
+
+
+m6 <- garchFit(~arma(1, 0) + garch(1, 1), data = adjcp, trace = F, include.mean = F, cond.dist = "sstd")
+summary(m6)
+plot(m6)
+
+
+
+
+
+
+
+############另外的代码
+data <- read.csv("08年每月.csv")
+library(forecast)
+Monthly_Price<-data$收盘
+library(tseries)
+Monthly_Pricets<-ts(Monthly_Price,frequency = 12,start=c(2008,1))
+fit_ets<-ets(Monthly_Pricets)
+fit_ets
+plot(forecast(fit_ets),xlab="Time",flty=2)
+accuracy(fit_ets)
